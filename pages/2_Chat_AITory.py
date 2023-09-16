6c826be5b3cefaa32367d4600adfb0c982991b97
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-09-16 23:45:36 +0900

    Update 2_Chat_AITory.py

--- pages/2_Chat_AITory.py
+++ pages/2_Chat_AITory.py
@@ -121,7 +121,8 @@ if pdf is not None:
             llm = OpenAI(
                 temperature=0.1,
                 model_name="gpt-3.5-turbo-16k",
-                top_p=1
+                top_p=1,
+                max_tokens=1000
             )
 
             chain = load_qa_chain(llm=llm, chain_type="stuff")
