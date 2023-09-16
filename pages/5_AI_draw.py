0657beaae1e23df5144b204490eb48816c892081
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-09-17 01:50:28 +0900

    Update 5_AI_draw.py

--- pages/5_AI_draw.py
+++ pages/5_AI_draw.py
@@ -1,30 +1,12 @@
-import os
 #  streamlit = Python에서 GUI 생성
 import streamlit as st
-import pickle # 파이썬 객체를 바이너리 파일로 저장하고 불러오는 기능
-import playsound
-
-from tempfile import NamedTemporaryFile
-
-from gtts import gTTS
 from dotenv import load_dotenv # OPEN_API_KEY
 
 from streamlit_extras.add_vertical_space import add_vertical_space
 from streamlit_chat import message
 
-# PyPDF2 = streamlit의 PDF 업로드를 읽기 위해 
-from PyPDF2 import PdfReader
-
-# langchain.text_splitter = PyPDF2의 텍스트를 chunks로 나눔
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-# openAI의 embedding = 계산하고 반환
-from langchain.embeddings.openai import OpenAIEmbeddings
-# VectorStore = FAISS, Chroma X = VectorStore에서 duckdb.DuckDBPyConnection 접근 불가능
-from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain # 답변
-
 import side_bar
+import openai
 
 # .env 파일로부터 환경 변수 로드
 load_dotenv()
@@ -33,7 +15,7 @@ load_dotenv()
 side_bar.run_side_bar()
 
 # 스트림릿 앱 헤더 설정
-st.header("AI Tory와 대화하기! 💬")
+st.header("AI Tory와 그림그리기")
 
 
 DrawButton = st.button("그려줘")
@@ -44,7 +26,33 @@ if DrawButton:
     else:
         conversation = ""
         for i in range(len(st.session_state['generated']) - 1, -1, -1):
-            conversation += f"사용자: {st.session_state['past'][i]}\n"
-            conversation += f"AI Tory: {st.session_state['generated'][i]}\n\n"
+            conversation += f"{st.session_state['generated'][i]}\n\n"
         st.write(conversation)
+        user_input = conversation
 
+
+        gpt_prompt = [{
+            "role" : "system",
+            "content" :  "Understand and paint Joaquin Soroya's artwork, soft natural light, {user_input}. Make it short."
+        }]
+
+        # gpt_prompt.append({
+        #     "role" : "user",
+        #     "content" : user_input
+        # })
+
+        with st.spinner("토리가 열심히 그림을 그리고 있어요.."):
+            gpt_response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=gpt_prompt
+            )
+
+        prompt = gpt_response["choices"][0]["message"]["content"]
+        st.write(prompt)
+
+        dallE_response = openai.Image.create(
+            prompt=prompt,
+            size="1024x1024",
+        )
+
+        st.image(dallE_response["data"][0]["url"])
