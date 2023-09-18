4f3d9dafcd73a72806c0ed287521392949473e76
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-09-18 22:56:46 +0900

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import side_bar
-st.title('README')
+st.title('Homepage')
 
 side_bar.run_side_bar()
+# https://scribehow.com/workspace#dashboard 수정 필요
+
+# 외부 웹 페이지의 URL
+iframe_url = "https://scribehow.com/embed/How_to_Use_AITory_to_Chat_Draw_and_Send_Files__T-W8Y4MsS4O1fSR_Oo8LvA"
+
+with st.spinner('사용법 로딩...'):
+    # iframe을 마크다운 형식으로 출력
+    st.markdown(f'<iframe src="{iframe_url}" width="100%" height="640" allowfullscreen frameborder="0"></iframe>', unsafe_allow_html=True)
