f7cbb7071c609f1e449c59b15e68971cd30393a0
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-09-18 22:56:35 +0900

    Update side_bar.py

--- side_bar.py
+++ side_bar.py
@@ -5,14 +5,10 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 # 스트림릿의 사이드바 설정
 def run_side_bar():
     with st.sidebar:
-        st.title('🤖 LLM AI Tory')
+        st.title('🤖 AI Tory')
         st.markdown('''
         ## About
-        AI Tory와 함께 놀아요! 😀
-        - [OpenAI](https://platform.openai.com/docs/models) LLM model
+        - Notion [AI Tory](https://platform.openai.com/docs/models)
+        - Github [AI Tory](https://python.langchain.com/)
         ''')
-
-        add_vertical_space(5)
-        st.write('Project [AI Tory](https://python.langchain.com/)')
-
         st.sidebar.success('성공')
