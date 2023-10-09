e4bbee6f3826292c56b1079f23ebc720568739ed
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-10-09 19:33:42 +0900

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,22 +1,28 @@
 import streamlit as st
-import side_bar
-st.title('Homepage')
+from side_bar import run_side_tap_home
 
-side_bar.run_side_bar()
+tory_image_path = "pages/images/tory.png" 
 
+run_side_tap_home()
+
+st.title(':blue[AI Tory] 🤖')
+st.subheader('| ChatGPT 기반의 인공지능 동화 스토리봇')
+st.subheader('', divider='gray')
 python_badge_url = "https://img.shields.io/badge/Python-3776AB.svg?&style=for-the-badge&logo=Python&logoColor=white"
 streamlit_badge_url = "https://img.shields.io/badge/Streamlit-FF4B4B?style=for-the-badge&logo=streamlit&logoColor=white"
 openai_badge_url = "https://img.shields.io/badge/OpenAI-412991.svg?&style=for-the-badge&logo=openai&logoColor=white"
-fireabse_badge_url = "https://img.shields.io/badge/Firebase-FFCA28.svg?&style=for-the-badge&logo=firebase&logoColor=white"
+firebase_badge_url = "https://img.shields.io/badge/Firebase-FFCA28.svg?&style=for-the-badge&logo=firebase&logoColor=white"
 
-# "https://img.shields.io/badge/기술명-원하는색상코드.svg?&style=for-the-badge&logo=로고명&logoColor=로고색상"
-# 파이썬, GitHub, 랭체인, 스트림릿 뱃지를 1줄로 정렬하여 스트림릿 앱에 추가
-st.image([python_badge_url, streamlit_badge_url, openai_badge_url, fireabse_badge_url])
+# 이미지를 중앙으로 정렬하는 HTML 및 CSS를 사용합니다.
+centered_image_html = f"""
+    <div style="display: flex; justify-content: center; padding-bottom: 20px">
+        <img src="{python_badge_url}" alt="Python Badge" style="max-width: 100%; padding: 20px;"/>
+        <img src="{streamlit_badge_url}" alt="Streamlit Badge" style="max-width: 100%; padding: 20px;"/>
+        <img src="{openai_badge_url}" alt="OpenAI Badge" style="max-width: 100%; padding: 20px;"/>
+        <img src="{firebase_badge_url}" alt="Firebase Badge" style="max-width: 100%; padding: 20px;"/>
+    </div>
+"""
+st.write(centered_image_html, unsafe_allow_html=True)
 
-# https://scribehow.com/workspace#dashboard 수정 필요
-# 외부 웹 페이지의 URL
 iframe_url = "https://scribehow.com/embed/How_to_Use_AITory_to_Chat_Draw_and_Send_Files__T-W8Y4MsS4O1fSR_Oo8LvA"
-
-with st.spinner('사용법 로딩...'):
-    # iframe을 마크다운 형식으로 출력
-    st.markdown(f'<iframe src="{iframe_url}" width="100%" height="640" allowfullscreen frameborder="0"></iframe>', unsafe_allow_html=True)
+st.markdown(f'<iframe src="{iframe_url}" width="100%" height="640" allowfullscreen frameborder="0"></iframe>', unsafe_allow_html=True)
