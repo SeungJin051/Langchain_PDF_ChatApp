22f9b13d46f168a88a4ab9de442bfc9b0c8fa076
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-10-12 11:35:33 +0900

    Update 2_AI_Tory_Draw.py

--- pages/2_AI_Tory_Draw.py
+++ pages/2_AI_Tory_Draw.py
@@ -2,6 +2,9 @@ import os
 import streamlit as st #  streamlit = Python에서 GUI 생성
 import openai
 import googletrans
+import playsound
+import pyaudio
+import wave
 
 from side_bar import run_side_tap_draw
 from dotenv import load_dotenv # OPEN_API_KEY
@@ -14,10 +17,11 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # 사이드 바 생성
 run_side_tap_draw()
-
+sample_rate = 44100  # 오디오 샘플 속도
+duration = 6  # 녹음 시간 (초)
 
 # 스트림릿 앱 헤더 설정
-st.header("AI Tory의 그림 그리기 🎨")
+st.header("그림 그리기 🎨 AI Tory가 그림을 그려줄게요 🪄")
 st.caption('토리가 학습한 정보로 그림을 그려줄게요 🔥')
 
 st.markdown("""
@@ -34,9 +38,40 @@ if not st.session_state.get('text') or st.session_state.text == []:
 # st.write(st.session_state.get('text'))
 
 if st.session_state.get('text'):
-    st.subheader("그림 그리기 🎨 AI Tory가 그림을 그려줄게요 🪄")
     query = st.text_input("원하는 그림에 대해서 설명 해주세요!", placeholder="Send a message")
-    # "그려줘" 버튼을 눌렀을 때 대화 내용 출력
+    whisper_button = st.button("🎙️", help="마이크를 연결해주세요.", key="unique_key_for_whisper_button")
+
+    if whisper_button:
+                    with st.spinner("말해주세요! 토리가 듣고있어요..."):
+                        # PyAudio를 사용하여 오디오 스트림 열기
+                        audio_data = []
+                        p = pyaudio.PyAudio()
+                        stream = p.open(format=pyaudio.paInt16, channels=1, rate=sample_rate, input=True, frames_per_buffer=1024)
+
+                        # 오디오 데이터 녹음
+                        for i in range(0, int(sample_rate / 1024 * duration)):
+                            audio_chunk = stream.read(1024)
+                            audio_data.append(audio_chunk)
+
+                    with st.spinner("토리가 다 들었어요..."):
+                        # 녹음 중지
+                        stream.stop_stream()
+                        stream.close()
+                        p.terminate()
+
+                        # 녹음된 오디오를 파일로 저장 (옵션)
+                        audio_file = "recorded_audio.wav"
+                        with wave.open(audio_file, "wb") as wf:
+                            wf.setnchannels(1)
+                            wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+                            wf.setframerate(sample_rate)
+                            wf.writeframes(b"".join(audio_data))
+
+                        # 수정된 부분: 녹음된 오디오 파일을 읽기 모드로 열기
+                        with open("recorded_audio.wav", "rb") as audio_file:
+                            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+                            ko_response = transcript["text"].encode('utf-16').decode('utf-16')
+                            query = ko_response
     if query:
             conversation = ""
             conversation += f"{st.session_state['text']}"
@@ -44,10 +79,14 @@ if st.session_state.get('text'):
 
             gpt_prompt = [{
                 "role" : "system", 
-                "content" : f"""In {user_input}, you are Beatrix Potter drawing. 
-                            I want to draw a children's storybook about {query}. Imagine the details.
+                "content" : f"""
+                            [Information] = {user_input}.
+                            You are Beatrix Potter.
+                            You are draw a children's storybook about {query}.
+                            Imagine the details.
+                            You Must be drawing for children's.
+                            If say something that's not in the [Information], just draw your own picture of {query}
                             """
-
             }]
             
             # gpt_prompt.append({
@@ -64,7 +103,7 @@ if st.session_state.get('text'):
 
             pic_prompt = gpt_response["choices"][0]["message"]["content"]
             dalle_prompt = pic_prompt
-            st.info(dalle_prompt)
+
             with st.spinner("토리가 동화에 대해서 그려줄게요.."):
                 dallE_response = openai.Image.create(
                     prompt=dalle_prompt,
